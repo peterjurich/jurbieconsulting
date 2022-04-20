13e16182b5627ed370b3d06305a210f0315099c9
Emily R Mabie <77937553+EmilyMabie@users.noreply.github.com>
2022-04-20 18:44:11 -0500

    Add files via upload

--- UnderstandingTaxBrackets/script.js
+++ UnderstandingTaxBrackets/script.js
@@ -88,27 +88,29 @@ function clearStatusClass(element) {
 
 const questions = [
     {
-        question: "What is 2+2?",
+        question: "Toby earned a yearly salary of $40,525 - the exact limit of the 12% tax bracket. Then, his boss gave him a raise of $1000. Which of the following is true?",
         answers: [
-            { text: "4", correct: true },
-            { text: "22", correct: false }
+            { text: "Toby will be taxed at 12% on the $40,525, and 22% on the $1000.", correct: true },
+            { text: "Toby will be taxed at 0% on the $40,525, and 22% on the $1000.", correct: false },
+            { text: "Toby will be taxed at 12% on the $40,525, and 0% on the $1000.", correct: false },
+            { text: "Toby will be taxed at 22% on the entire $41,525.", correct: false }
         ]
     },
     {
-        question: "Who is the bab?",
+        question: "Emily was making a yearly salary of $9,950, and because this fell within the 10% tax bracket, her taxes totaled $995. The next year, thanks to a $2000 raise, she's making $11,950. Only $2000 is taxed at %12, so how much total tax will she pay?",
         answers: [
-            { text: "Scremblay", correct: false },
-            { text: "Toby", correct: true },
-            { text: "Peter", correct: false },
-            { text: "Ollie", correct: true }
+            { text: "$2995 - so the raise isn't worth it", correct: false },
+            { text: "10% of $9,950, minus 12% of $2,000 = $755 total taxes", correct: false },
+            { text: "$995 (from last year's taxes) multiplied by 12% = $119.4", correct: false },
+            { text: "10% of $9,950, plus 12% of $2,000 = $1235 total taxes", correct: true }
         ]
     },
     {
-        question: "If Oliver Waffles' salary is $87,000 per year, how much of his salary is taxed at 24%?",
+        question: "If Oliver's salary is $87,000 per year, how much of his salary is taxed at 24%?",
         answers: [
             { text: "-$86,376", correct: false },
             { text: "$20,880", correct: false },
-            { text: "$624", correct: true },
+            { text: "$625", correct: true },
             { text: "$87,000", correct: false }
         ]
     }
